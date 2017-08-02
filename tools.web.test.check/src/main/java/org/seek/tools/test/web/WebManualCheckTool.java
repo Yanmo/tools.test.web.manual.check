5e2035b756e2b36d6441217f77476952522766d1
Yasuhiro Yamamoto <yasuhiro.y0807@gmail.com>
2017-08-02 22:18:36 +0900

    move some files.

---
+++ tools.web.test.check/src/main/java/org/seek/tools/test/web/WebManualCheckTool.java
@@ -0,0 +1,13 @@
+package org.seek.tools.test.web;
+
+
+
+public class WebManualCheckTool {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World.");
+	}
+
+}
+	
